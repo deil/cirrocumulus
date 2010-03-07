b2ded10cda1b4c294ce1bc57640a5bd12f03c4d6
deil <deil@mneko.net>
2010-03-07 22:54:30 +0300

    groupchat

--- agent/src/cirrocumulus.erl
+++ agent/src/cirrocumulus.erl
@@ -37,6 +37,7 @@
 -define(Server, "89.223.109.31").
 -define(Port, 5222).
 -define(Hostname, "hc001.o1host.net").
+-define(Chatroom, "cirrocumulus@conference.o1host.net/" ++ hostname()).
 
 start() ->
     spawn(?MODULE, init, []).
@@ -84,6 +85,11 @@ session(MySession, _MyJID) ->
     exmpp_session:send_packet(MySession,
 			      exmpp_presence:set_status(
 				exmpp_presence:available(), "Cirrocumulus")),
+				
+    %% Join groupchat
+    Packet1 = #xmlel{name = 'presence'},
+    Packet = exmpp_xml:set_attribute(Packet1, to, ?Chatroom),
+    exmpp_session:send_packet(MySession, Packet),
     loop(MySession).
 
 %% Process exmpp packet:
@@ -103,9 +109,43 @@ loop(MySession) ->
 
 %% Send the same packet back for each message received
 echo_packet(MySession, Packet) ->
+    An1 = #xmlel{name = 'iq'},
+    An2 = exmpp_xml:set_attribute(An1, type, <<"set">>),
+    An3 = exmpp_xml:set_attribute(An2, to, "o1host.net"),
+    An4 = exmpp_xml:set_attribute(An3, id, '666'),
+    Cmd1 = #xmlel{ns = "http://jabber.org/protocol/commands", name = "command"},
+    %%Cmd2 = exmpp_xml:declare_ns_here(Cmd1, "http://jabber.org/protocol/commands", none),
+    Cmd3 = exmpp_xml:set_attribute(Cmd1, node, <<"http://jabber.org/protocol/admin#announce">>),
+    X1 = #xmlel{ns = "jabber:x:data", name = "x"},
+    %%X2 = exmpp_xml:declare_ns_here(X1, "jabber:x:data", none),
+    X3 = exmpp_xml:set_attribute(X1, type, <<"submit">>),
+    Field1 = exmpp_xml:element("field"),
+    Field2 = exmpp_xml:set_attribute(Field1, type, <<"hidden">>),
+    Field3 = exmpp_xml:set_attribute(Field2, var, <<"FORM_TYPE">>),
+    Val1 = exmpp_xml:element("value"),
+    Val2 = exmpp_xml:set_cdata(Val1, <<"http://jabber.org/protocol/admin">>),
+    Field4 = exmpp_xml:append_child(Field3, Val2),
+    Field5 = exmpp_xml:element("field"),
+    Field6 = exmpp_xml:set_attribute(Field5, type, <<"text-single">>),
+    Field7 = exmpp_xml:set_attribute(Field6, var, <<"subject">>),
+    Val3 = exmpp_xml:element("value"),
+    Val4 = exmpp_xml:set_cdata(Val3, <<"subj">>),
+    Field8 = exmpp_xml:append_child(Field7, Val4),
+    Field9 = exmpp_xml:element("field"),
+    Field10 = exmpp_xml:set_attribute(Field9, type, <<"text-multi">>),
+    Field11 = exmpp_xml:set_attribute(Field10, var, <<"body">>),
+    Val5 = exmpp_xml:element("value"),
+    Val6 = exmpp_xml:set_cdata(Val5, <<"body">>),
+    Field12 = exmpp_xml:append_child(Field11, Val6),
+    X4 = exmpp_xml:append_child(X3, Field4),
+    X5 = exmpp_xml:append_child(X4, Field8),
+    X6 = exmpp_xml:append_child(X5, Field12),
+    Cmd4 = exmpp_xml:append_child(Cmd3, X6),
+    An = exmpp_xml:append_child(An4, Cmd4),
     From = exmpp_xml:get_attribute(Packet, from, <<"unknown">>),
     To = exmpp_xml:get_attribute(Packet, to, <<"unknown">>),
     TmpPacket = exmpp_xml:set_attribute(Packet, from, To),
-    TmpPacket2 = exmpp_xml:set_attribute(TmpPacket, to, From),
+    TmpPacket2 = exmpp_xml:set_attribute(TmpPacket, to, <<"cirrocumulus@conference.o1host.net">>),
     NewPacket = exmpp_xml:remove_attribute(TmpPacket2, id),
+    io:format("packet: ~s", [exmpp_xml:document_to_binary(NewPacket)]),
     exmpp_session:send_packet(MySession, NewPacket).
