bf03d2eacae14d061ac0b9b01e1d638f7d5484a4
root <root@s001s1.mneko.net>
2010-03-23 16:59:01 +0300

    updated zabbix script to comply FIPA ACL message structure

--- scripts/zabbix.rb
+++ scripts/zabbix.rb
@@ -3,9 +3,10 @@
 require 'rubygems'
 require 'xmpp4r-simple'
 
+body = "<fipa-message act=\"inform\" ontology=\"http://api.o1host.net/ontology/cirrocumulus/zabbix\"><sender name=\"zabbix@o1host.net\"/><content>(inform (msg \"#{ARGV[2]}\")</content></fipa-message>"
 jabber = Jabber::Simple.new('zabbix@o1host.net', "zabbix")
 jabber.send!("<presence to='cirrocumulus@conference.o1host.net/zabbix' />")
-jabber.send!("<message to='cirrocumulus@conference.o1host.net' type='groupchat'><body>#{ARGV[2]}</body></message>")
+jabber.send!("<message to='cirrocumulus@conference.o1host.net' type='groupchat'><body>#{body.gsub('"', '&quot;').gsub('<', '&lt;').gsub('>', '&gt;')}</body></message>")
 jabber.disconnect
 
 
