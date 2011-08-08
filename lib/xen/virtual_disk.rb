8cb0a27c33566266f810768ccf4840b52f298a2b
deil <deil@mneko.net>
2011-08-08 23:51:33 +0400

    xen: renamed virtual disk states to more accurate

--- lib/xen/virtual_disk.rb
+++ lib/xen/virtual_disk.rb
@@ -2,9 +2,9 @@ class VirtualDisk
   def self.check_state(disk_number)
     _, res = systemu "cat /proc/mdstat | grep md#{disk_number}"
     lines = res.split("\n")
-    return :stopped if lines.blank?
+    return :disconnected if lines.blank?
 
     line = lines.first
-    return line.split(" ")[2] == "active" ? :active : :failed
+    return line.split(" ")[2] == "active" ? :connected : :failed
   end
 end
