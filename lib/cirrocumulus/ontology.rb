cdbc9474597e246ee84c19cb05ffa1a829f709c2
deil <deil@mneko.net>
2011-09-16 23:33:02 +0400

    returned back sagas

--- lib/cirrocumulus/ontology.rb
+++ lib/cirrocumulus/ontology.rb
@@ -6,6 +6,7 @@ module Ontology
     def initialize(name, agent)
       @name = name
       @agent = agent
+      @sagas = []
     end
 
     def restore_state()
@@ -13,13 +14,37 @@ module Ontology
     end
 
     def tick()
+      @sagas.each do |saga|
+        next if saga.is_finished?
+        saga.timeout -= 1 if saga.timeout > 0
+        saga.handle(nil) if saga.timeout == 0
+      end
+
+      handle_tick()
+    end
+
+    def handle_incoming_message(message, kb)
+      was_processed = false
+      @sagas.each do |saga|
+        next if saga.is_finished?
+
+        if saga.id == message.in_reply_to
+          was_processed = true
+          saga.handle(message)
+        end
+      end
+
+      handle_message(message, kb) if !was_processed
+    end
+
+    protected
+
+    def handle_tick()
     end
 
     def handle_message(message, kb)
       puts "call to dummy Ontology::Base.handle_message()"
     end
-
-    protected
 
     def logger
       Log4r::Logger['agent']
